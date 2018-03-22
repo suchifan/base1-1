178c22e842248adf776904ccca78cb509b80ef7d
suchifan <sc@smail.nju.edu.cn>
2018-03-22 16:13:09 +0800

    first commit

---
+++ base1-1/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  base1-1
+//
+//  Created by suchao on 3/22/18.
+//  Copyright © 2018 cs.nju. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "(?-?)" << endl;
+    cout << "It means puzzle." << endl;
+    return 0;
+}
